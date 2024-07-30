30ac4be130a942075ce842132f30062e4d7a8518
Arjungowda1 <arjunce15@gmail.com>
2024-07-30 09:31:42 +0530

    test run on ec2

---
+++ mailconfig.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_ID,
+      pass: process.env.MAIL_KEY,
+    }}
+);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log('Error with transporter configuration:', error);
+  } else {
+    console.log('Server is ready to take messages:', success);
+  }
+});
+
+
+app.post('/email', (req, res) => {
+  const { name, from, subject, message  } = req.body;
+
+  const text = "A mail from: " + from + "\n\n" + "and it reads:\n" + message;
+
+  const mailOptions = {
+    from: {
+        name: name,
+        address: process.env.MAIL_ID 
+    }, 
+    to: "arjunce15@gmail.com",
+    subject: subject, 
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send({ success: false, message: 'Failed to send email', error });
+    }
+    res.status(200).send({ success: true, message: 'Email sent successfully', info });
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Email service listening at http://localhost:${port}`);
+});
